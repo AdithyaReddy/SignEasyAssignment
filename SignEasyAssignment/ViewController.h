b168e4b1b7939654cc606d767a5dd604fec3362c
Adithya Reddy <adithya@frankly.me>
2015-10-26 23:58:34 +0530

    Initial Commit

---
+++ SignEasyAssignment/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SignEasyAssignment
+//
+//  Created by Adithya Reddy on 26/10/15.
+//  Copyright © 2015 Adithya Reddy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SignEasyAssignment/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SignEasyAssignment
+//
+//  Created by Adithya Reddy on 26/10/15.
+//  Copyright © 2015 Adithya Reddy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
